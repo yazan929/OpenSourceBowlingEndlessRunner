f1421eedbc98db3093b6c57a90f01a6d5ad1c410
tk3vin <68420820+tk3vin@users.noreply.github.com>
2021-06-28 13:58:56 +0800

    Fixed Main Menu buttons

---
+++ Bowling Endless Runner/Assets/menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+
+    public void Play(){
+        SceneManager.LoadScene ("Level0");
+    }
+    
+}
